af54b33a7df0ccbbd2839f42cb5da9f558ad6fa7
Girish Kulkarni <kulkarni.girish60@gmail.com>
2023-10-31 18:59:33 +0530

    1_27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,23 @@
 # pcost.py
 #
 # Exercise 1.27
+
+def get_a_stock(): 
+    with open('Data/portfolio.csv', 'rt') as f:
+        next(f)
+        for line in f:
+            line = line.strip()
+            line = line.split(',')
+            yield line
+
+total_cost = 0
+
+for _, n_stock, stock_price in get_a_stock():
+    # print(n_stock, stock_price)
+    total_cost += int(n_stock) * float(stock_price)
+
+print('Total Cost ', total_cost)
+
+
+
+

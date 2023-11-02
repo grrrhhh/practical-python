5c68c55e029b97830405a68083768bf55c717583
Girish Kulkarni <kulkarni.girish60@gmail.com>
2023-11-02 15:41:01 +0530

    2_11, 2_12

--- Work/report.py
+++ Work/report.py
@@ -51,15 +51,39 @@ def calculate_p_l(portfolio, ticker):
 
     return p_l
 
+def make_report(portfolio, ticker):
+    report = []
+
+    for holding in portfolio:
+        row = []
+        symbol = holding['name']
+        row.append(symbol)
+        row.append(holding['shares'])
+        row.append(ticker[symbol])
+        row.append(ticker[symbol] - holding['price'])
+        report.append(row)
+    
+    return report
+
 portfolio_source = 'Data/portfolio.csv'
 price_source = 'Data/prices.csv'
 
 portfolio = read_portfolio(portfolio_source)
-pprint(portfolio)
-
+# pprint(portfolio)
 prices = read_prices(price_source)
-pprint(prices)
+# pprint(prices)
 
 print('Portfolio Cost: ', portfolio_cost(portfolio_source) )
 print('P/L:', calculate_p_l(portfolio, prices) )
+
+report = make_report(portfolio, prices)
+
+headers = ('Name', 'Shares', 'Price', 'Change')
+print('%10s %10s %10s %10s' % headers)
+print(('-' * 10 + ' ') * len(headers))
+
+for name, shares, price, change in report:
+    price = "$" + f'{price:.2f}'
+    print(f"{name:>10s} {shares:>10d} {price:>10s} {change:>10.2f}")
+
 

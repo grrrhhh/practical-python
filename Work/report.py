21ba40cc864d3c0af367d602b3e361dbcd24bb0d
Girish Kulkarni <kulkarni.girish60@gmail.com>
2023-11-03 17:53:09 +0530

    more functions

--- Work/report.py
+++ Work/report.py
@@ -2,7 +2,6 @@
 #
 # Exercise 2.4
 import csv
-from pprint import pprint
 
 def read_portfolio(filename):
     portfolio = []
@@ -63,25 +62,31 @@ def make_report(portfolio, ticker):
     
     return report
 
+def print_report(report):
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print('%10s %10s %10s %10s' % headers)
+    print(('-' * 10 + ' ') * len(headers))
+
+    for name, shares, price, change in report:
+        price = "$" + f'{price:.2f}'
+        print(f"{name:>10s} {shares:>10d} {price:>10s} {change:>10.2f}")
+
+
+def portfolio_report(f_portfolio, f_prices):
+    portfolio = read_portfolio(f_portfolio)
+    prices = read_prices(f_prices)
+    print_report(make_report(portfolio, prices))
+
 portfolio_source = 'Data/portfolio.csv'
 price_source = 'Data/prices.csv'
 
 portfolio = read_portfolio(portfolio_source)
-# pprint(portfolio)
 prices = read_prices(price_source)
-# pprint(prices)
 
 print('Portfolio Cost: ', portfolio_cost(portfolio_source) )
 print('P/L:', calculate_p_l(portfolio, prices) )
 
-report = make_report(portfolio, prices)
+# report = make_report(portfolio, prices)
+# print_report(report)
 
-headers = ('Name', 'Shares', 'Price', 'Change')
-print('%10s %10s %10s %10s' % headers)
-print(('-' * 10 + ' ') * len(headers))
-
-for name, shares, price, change in report:
-    price = "$" + f'{price:.2f}'
-    print(f"{name:>10s} {shares:>10d} {price:>10s} {change:>10.2f}")
-
-
+portfolio_report('Data/portfolio.csv', 'Data/prices.csv')

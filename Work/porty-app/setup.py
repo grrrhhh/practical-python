313fded91c895c6682c05cfe9f4c1548ce01b47b
Girish Kulkarni <kulkarni.girish60@gmail.com>
2023-11-17 10:41:34 +0530

    9_5 packaged porty

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Girish",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
